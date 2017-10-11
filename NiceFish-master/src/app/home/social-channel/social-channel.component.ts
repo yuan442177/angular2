11f862e6c8a41bd4d16cc4445db6ecb413194a84
yuan442177 <15973370670@163.com>
2017-10-11 17:20:04 +0800

    first commit

---
+++ NiceFish-master/src/app/home/social-channel/social-channel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({//装饰器
+  selector: 'app-social-channel',//定义标签
+  templateUrl: './social-channel.component.html',//定义组件模板路径
+  styleUrls: ['./social-channel.component.scss']
+})
+export class SocialChannelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+}
